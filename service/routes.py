b7a086dd27fee72036daf8694bdc2b825761bb65
sandyshao <sandyshaoxinyi@gmail.com>
2023-10-14 12:21:35 -0400

    initial check in for issue-5

--- service/routes.py
+++ service/routes.py
@@ -1,12 +1,13 @@
 """
-My Service
+Product Service
 
-Describe what your service does here
+Paths:
+PUT /products/{product_id} - Updates a Product record in the database
 """
 
 from flask import jsonify, request, url_for, abort
 from service.common import status  # HTTP Status Codes
-from service.models import YourResourceModel
+from service.models import Product
 
 # Import Flask application
 from . import app
@@ -27,5 +28,22 @@ def index():
 ######################################################################
 #  R E S T   A P I   E N D P O I N T S
 ######################################################################
+@app.route("/products/<int:product_id>", methods=["PUT"])
+def update_product(product_id):
+    """
+    Update a Product
 
-# Place your REST API code here ...
+    This endpoint will update a Product based the body that is posted
+    """
+    app.logger.info("Request to update product with id: %s", product_id)
+
+    product = Product.find(product_id)
+    if not product:
+        abort(status.HTTP_404_NOT_FOUND, f"Pet with id '{product_id}' was not found.")
+
+    product.deserialize(request.get_json())
+    product.id = product_id
+    product.update()
+
+    app.logger.info("Product with ID [%s] updated.", product.id)
+    return jsonify(product.serialize()), status.HTTP_200_OK
