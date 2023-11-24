1c84428425268e6a67bcd9ddbda4d3e327743e81
jalam7 <81439372+jalam7@users.noreply.github.com>
2023-11-24 01:17:34 +0000

    refactored the routes with flask_restx.

--- service/__init__.py
+++ service/__init__.py
@@ -6,16 +6,34 @@ and SQL database
 """
 import sys
 from flask import Flask
+from flask_restx import Api
 from service import config
 from service.common import log_handlers
+
 
 # Create Flask application
 app = Flask(__name__)
 app.config.from_object(config)
 
+######################################################################
+# Configure Swagger before initializing it
+######################################################################
+api = Api(
+    app,
+    version="1.0.0",
+    title="Products REST API Service",
+    description="This is a sample Products microservice.",
+    default="products",
+    default_label="Products Microservice API",
+    doc="/apidocs",  # default also could use doc='/apidocs/'
+    # authorizations=authorizations,
+    prefix="/api",
+)
+
 # Dependencies require we import the routes AFTER the Flask app is created
 # pylint: disable=wrong-import-position, wrong-import-order, cyclic-import
 from service import routes, models  # noqa: E402, E261
+
 # pylint: disable=wrong-import-position
 from service.common import error_handlers, cli_commands  # noqa: F401, E402
 
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -18,7 +18,7 @@ from tests.factories import ProductFactory
 DATABASE_URI = os.getenv(
     "DATABASE_URI", "postgresql://postgres:postgres@localhost:5432/testdb"
 )
-BASE_URL = "/products"
+BASE_URL = "/api/products"
 
 
 ######################################################################
